e5ad0cc6e5a758ca6c9c1d9a62e875fe8db143d9
Suraj <kumarsuraj54057@gmail.com>
2024-03-23 14:50:11 +0530

    step section

---
+++ src/Components/StepsSection/StepSection.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./StepSection.css";
+import stepImage from "../../Assets/Images/arrows-1.svg";
+
+const StepSection = () => {
+  return (
+    <div className="stepSection-container">
+      <div className="step-container container">
+        <div className="step-content">
+          <p className="step-num">Step 1</p>
+          <p className="step-title">Get quick help with an expert</p>
+          <p className="step-peragraph">
+            Let us know where you’re going by explaining everything via phone.
+            You can also do everything via email for your convenience.
+          </p>
+        </div>
+        <div className="step-arrow">
+          <img src={stepImage} alt="" />
+        </div>
+        <div className="step-content">
+          <p className="step-num">Step 2</p>
+          <p className="step-title">Receive options via email</p>
+          <p className="step-peragraph">
+            Get expert itineraries within minutes in your inbox. You can adjust
+            any layover or pick airline. Your Travel Manager will do it for you.
+          </p>
+        </div>
+        <div className="step-arrow">
+          <img src={stepImage} alt="" />
+        </div>
+        <div className="step-content">
+          <p className="step-num">Step 3</p>
+          <p className="step-title">Choose Offer and travel</p>
+          <p className="step-peragraph">
+            Pay securely online, make any adjustments, receive your e-tickets
+            via email and you are ready to fly!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StepSection;
--- src/Pages/SearchedFlightPage/SearchedFligh.jsx
+++ src/Pages/SearchedFlightPage/SearchedFligh.jsx
@@ -4,17 +4,21 @@ import MainForm from "../../Components/MainFormSection/MainForm";
 // import mainImage from "../../Assets/Images/deeplink-bg.jpg";
 import Header from "../../Components/Header/Header";
 import RecentDeals from "../../Components/RecentCheapDeals/RecentDeals";
+import StepSection from "../../Components/StepsSection/StepSection";
 
 const SearchedFligh = () => {
   return (
     <div className="searchedFlight">
       <div className="searchedFlightContainer">
-      {/* <img className="mainImage" src={mainImage} alt="" /> */}
+        {/* <img className="mainImage" src={mainImage} alt="" /> */}
         <Header />
         <MainForm />
       </div>
       <div className="recentCheapDealsSection">
         <RecentDeals />
+      </div>
+      <div className="stepSection">
+        <StepSection />
       </div>
     </div>
   );
