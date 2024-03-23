e6da2bfbf00e2cf496b89fa674b29912293d2950
Suraj <kumarsuraj54057@gmail.com>
2024-03-23 12:22:47 +0530

    error fix

--- src/Pages/SearchedFlightPage/SearchedFligh.jsx
+++ src/Pages/SearchedFlightPage/SearchedFligh.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./SearchedFligh.css";
 import MainForm from "../../Components/MainFormSection/MainForm";
-import mainImage from "../../Assets/Images/deeplink-bg.jpg";
+// import mainImage from "../../Assets/Images/deeplink-bg.jpg";
 import Header from "../../Components/Header/Header";
 import RecentDeals from "../../Components/RecentCheapDeals/RecentDeals";
 
