ba4e3add5023b03ae28186c78691d367d90a4787
Suraj <kumarsuraj54057@gmail.com>
2024-03-24 13:17:18 +0530

    styling

--- src/Components/RecentCheapDeals/RecentDeals.jsx
+++ src/Components/RecentCheapDeals/RecentDeals.jsx
@@ -3,6 +3,33 @@ import "./RecentDeals.css";
 import { BsCurrencyDollar } from "react-icons/bs";
 import flightimage from "../../Assets/Images/00.png";
 
+const cards = [
+  {
+    id: 1,
+    card_title: "Popular",
+    flight_logo: flightimage,
+    destication: "NYC - ROM",
+    class: "Economy,rt,total",
+    flight_price: "950",
+  },
+  {
+    id: 2,
+    card_title: "Fastest",
+    flight_logo: flightimage,
+    destication: "NYC - ROM",
+    class: "Economy,rt,total",
+    flight_price: "950",
+  },
+  {
+    id: 3,
+    card_title: "Best Seller",
+    flight_logo: flightimage,
+    destication: "NYC - ROM",
+    class: "Economy,rt,total",
+    flight_price: "950",
+  },
+];
+
 const RecentDeals = () => {
   return (
     <div className="recentDealsSection">
@@ -11,78 +38,42 @@ const RecentDeals = () => {
           <h3>Recent Cheap Flight Deals from New York City to Rome</h3>
         </div>
         <div className="recentCards">
-          <div className="recentCard">
-            <div className="card-contents">
-              <div className="card-title">
-                <h5>Popular</h5>
-              </div>
-              <div className="fromAndPrice">
-                <div className="flight-from">
-                  <div className="flight-logo">
-                    <img src={flightimage} alt="" />
-                  </div>
-                  <div className="flight-from-to">
-                    <div className="flight-cityname">
-                      <h4>NYC - ROM</h4>
-                      <p>Economy,rt,total</p>
+          {cards.map((card) => (
+            <div key={card.id} className="recentCard">
+              <div className="card-contents">
+                <div className="card-title">
+                  <h5
+                    style={
+                      card.id === 1
+                        ? { color: "orange" }
+                        : card.id === 3
+                        ? { color: "red" }
+                        : { color: "green" }
+                    }
+                  >
+                    {card.card_title}
+                  </h5>
+                </div>
+                <div className="fromAndPrice">
+                  <div className="flight-from">
+                    <div className="flight-logo">
+                      <img src={card.flight_logo} alt="" />
+                    </div>
+                    <div className="flight-from-to">
+                      <div className="flight-cityname">
+                        <h4>{card.destication}</h4>
+                        <p>{card.class}</p>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="flight-price">
-                  <BsCurrencyDollar />
-                  <strong>950</strong>
+                  <div className="flight-price">
+                    <BsCurrencyDollar />
+                    <strong>{card.flight_price}</strong>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="recentCard">
-            <div className="card-contents">
-              <div className="card-title">
-                <h5>Popular</h5>
-              </div>
-              <div className="fromAndPrice">
-                <div className="flight-from">
-                  <div className="flight-logo">
-                    <img src={flightimage} alt="" />
-                  </div>
-                  <div className="flight-from-To">
-                    <div className="flight-cityname">
-                      <h4>NYC - ROM</h4>
-                      <p>Economy,rt,total</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="flight-price">
-                  <BsCurrencyDollar />
-                  <strong>950</strong>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="recentCard">
-            <div className="card-contents">
-              <div className="card-title">
-                <h5>Popular</h5>
-              </div>
-              <div className="fromAndPrice">
-                <div className="flight-from">
-                  <div className="flight-logo">
-                    <img src={flightimage} alt="" />
-                  </div>
-                  <div className="flight-from-To">
-                    <div className="flight-cityname">
-                      <h4>NYC - ROM</h4>
-                      <p>Economy,rt,total</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="flight-price">
-                  <BsCurrencyDollar />
-                  <strong>950</strong>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
